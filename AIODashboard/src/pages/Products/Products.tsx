4f0e14fc8f450594a441cd3e100cb91b839dde92
Paul Christian <paulchristianmasayon2020@gmail.com>
2025-06-14 16:54:11 +0800

    Imported Table from Material UI and customized, created & moved types and interfaces that are too many per file to a separate .ts file in /types

--- AIODashboard/src/pages/Products/Products.tsx
+++ AIODashboard/src/pages/Products/Products.tsx
@@ -1,59 +1,11 @@
 import { useProducts } from "../../hooks/useProducts";
 
 import { CircularProgress, Box } from "@mui/material";
-
-export interface Product {
-  id: number;
-  title: string;
-  description: string;
-  category: string;
-  price: number;
-  discountPercentage: number;
-  rating: number;
-  stock: number;
-  tags: string[];
-  brand: string;
-  sku: string;
-  weight: number;
-  dimensions: {
-    width: number;
-    height: number;
-    depth: number;
-  };
-  warrantyInformation: string;
-  shippingInformation: string;
-  availabilityStatus: string;
-  reviews: Review[];
-  returnPolicy: string;
-  minimumOrderQuantity: number;
-  meta: {
-    createdAt: string; // ISO date string
-    updatedAt: string; // ISO date string
-    barcode: string;
-    qrCode: string;
-  };
-  thumbnail: string;
-  images: string[];
-}
-
-export interface Review {
-  rating: number;
-  comment: string;
-  date: string; // ISO date string
-  reviewerName: string;
-  reviewerEmail: string;
-}
-
-export interface ProductResponse {
-  products: Product[];
-  total: number;
-  skip: number;
-  limit: number;
-}
+import ProductsTable from "./ProductsTable";
 
 export default function Products() {
   const { data, isLoading, isError } = useProducts();
-  // const first20 = data.slice(0, 20);
+
   console.log("Products:", data && data);
   return (
     <div>
@@ -63,12 +15,7 @@ export default function Products() {
           <CircularProgress />
         </Box>
       ) : (
-        <>
-          {data &&
-            data.products.map((product: Product) => (
-              <li key={product.id}>{product.title}</li>
-            ))}
-        </>
+        <>{data && <ProductsTable {...data} />}</>
       )}
       {isError ? <>Error</> : ""}
     </div>
---
+++ AIODashboard/src/types/Products.ts
@@ -0,0 +1,78 @@
+// TYPES FOR RESPONSE DATA -----------------------------
+export interface ProductResponse {
+  products: Product[];
+  total: number;
+  skip: number;
+  limit: number;
+}
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  tags: string[];
+  brand: string;
+  sku: string;
+  weight: number;
+  dimensions: {
+    width: number;
+    height: number;
+    depth: number;
+  };
+  warrantyInformation: string;
+  shippingInformation: string;
+  availabilityStatus: string;
+  reviews: Review[];
+  returnPolicy: string;
+  minimumOrderQuantity: number;
+  meta: {
+    createdAt: string;
+    updatedAt: string;
+    barcode: string;
+    qrCode: string;
+  };
+  thumbnail: string;
+  images: string[];
+}
+export interface Review {
+  rating: number;
+  comment: string;
+  date: string;
+  reviewerName: string;
+  reviewerEmail: string;
+}
+
+// TYPES FOR TABLE USE -----------------------------
+export interface TableProduct {
+  id: number;
+  brand: string;
+  price: number;
+  description: string;
+  title: string;
+  rating: number;
+}
+export interface HeadCell {
+  disablePadding: boolean;
+  id: keyof TableProduct;
+  label: string;
+  numeric: boolean;
+}
+export interface EnhancedTableProps {
+  numSelected: number;
+  onRequestSort: (
+    event: React.MouseEvent<unknown>,
+    property: keyof TableProduct
+  ) => void;
+  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
+export interface EnhancedTableToolbarProps {
+  numSelected: number;
+}
+export type Order = "asc" | "desc";
