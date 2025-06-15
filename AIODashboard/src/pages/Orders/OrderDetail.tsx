5ed4c8eb053ab9baa3b2d0dca7263b4d1d2b2731
Paul Christian <paulchristianmasayon2020@gmail.com>
2025-06-16 03:22:46 +0800

    Improved conditional renders

--- AIODashboard/src/pages/Orders/OrderDetail.tsx
+++ AIODashboard/src/pages/Orders/OrderDetail.tsx
@@ -1,3 +1,46 @@
+import { Grid, Stack, Typography } from "@mui/material";
+import { useOrderDetail } from "../../hooks/useOrders";
+import { useParams } from "react-router-dom";
+import Spinner from "../../components/Spinner";
 export default function OrderDetail() {
-  return <div>OrderDetail</div>;
+  const { id } = useParams();
+  const { data, isLoading, isError } = useOrderDetail(id ? id : "");
+
+  console.log("OrderDetail:", data && data);
+
+  if (isLoading) {
+    return <Spinner text="Loading order info..." />;
+  }
+
+  if (isError) {
+    return (
+      <Typography variant="h6" color="error" sx={{ m: 4 }}>
+        Failed to load order details. Please try again later.
+      </Typography>
+    );
+  }
+
+  if (!data) {
+    return (
+      <Typography variant="h6" color="warning.main" sx={{ m: 4 }}>
+        No order data found.
+      </Typography>
+    );
+  }
+
+  return (
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        marginLeft: {
+          xs: "5vw",
+        },
+      }}
+    >
+      <Stack direction="column" spacing={1}>
+        <h1>Order Summary</h1>
+      </Stack>
+    </Grid>
+  );
 }
