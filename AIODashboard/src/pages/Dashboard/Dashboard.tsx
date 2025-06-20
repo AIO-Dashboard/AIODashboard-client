89f1eae2b293154ceac756b4edb3c4ecc1dae4ce
Paul Christian <paulchristianmasayon2020@gmail.com>
2025-06-20 18:59:07 +0800

    comment

--- AIODashboard/src/pages/Dashboard/Dashboard.tsx
+++ AIODashboard/src/pages/Dashboard/Dashboard.tsx
@@ -14,3 +14,94 @@ export default function Dashboard() {
     </>
   );
 }
+
+//
+//
+// ===========INTERVIEW
+// // import { Box, Grid } from "@mui/material";
+
+// // import Button from "@mui/material/Button";
+// // import { Link } from "react-router-dom";
+// import {
+//   // React,
+//   useEffect,
+//   useState,
+// } from "react";
+
+// export default function Dashboard() {
+//   const [data, setData] = useState([]);
+//   const [fileredData, setFileredData] = useState([]);
+//   const [searchKey, setSearchKey] = useState("");
+//   useEffect(() => {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((res) => res.json())
+//       .then((data) => setData(data));
+//   }, []);
+
+//   const handleSearch = (e: any) => {
+//     console.log(e.target.value);
+//     setSearchKey(e.target.value);
+//   };
+
+//   useEffect(() => {
+//     console.log(data);
+//     const filteredData = data.filter(
+//       (d: any) =>
+//         d.name.toLocaleLowerCase().includes(searchKey.toLocaleLowerCase())
+//       // d
+//     );
+//     setFileredData(filteredData);
+//     console.log(filteredData);
+//   }, [searchKey]);
+
+//   return (
+//     <>
+//       <input type="text" onChange={handleSearch} />
+//       <div className="App">
+//         {fileredData.length < 1 && data ? (
+//           data.map((d: any) => (
+//             <div className={"nameBox"} key={d.id}>
+//               {d.name}
+//             </div>
+//           ))
+//         ) : fileredData.length > 0 ? (
+//           fileredData.map((d: any) => (
+//             <div className={"nameBox"} key={d.id}>
+//               {d.name}
+//             </div>
+//           ))
+//         ) : (
+//           <div>No data found</div>
+//         )}
+//       </div>
+//     </>
+//   );
+// }
+
+// // export function App(props) {
+// //   return (
+// //     <>
+// //       <input type="text" onChange={handleSearch} />
+// //       <div className="App">
+// //         {fileredData.length < 1 && data ? (
+// //           data.map((d) => (
+// //             <div className={"nameBox"} key={d.id}>
+// //               {d.name}
+// //             </div>
+// //           ))
+// //         ) : fileredData.length > 1 ? (
+// //           data.map((d) => (
+// //             <div className={"nameBox"} key={d.id}>
+// //               {d.name}
+// //             </div>
+// //           ))
+// //         ) : (
+// //           <div>No data found</div>
+// //         )}
+// //       </div>
+// //     </>
+// //   );
+// // }
+
+// // Log to console
+// console.log("Hello console");
